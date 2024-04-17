03d3b36073fced76acd422da6fc6582fe1721b85
alex bedolla <undefindev@outlook.com>
2024-04-16 19:59:10 -0700

    creando el modelo de 'projecto'

---
+++ src/models/Project.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Document} from "mongoose";
+
+
+// este es el type de typeScript
+export type ProjectType = Document & {
+  projectName: string
+  clientName: string
+  desciption: string
+}
+
+// y este el schema
+const ProjectSchema: Schema = new Schema({
+  projectName: {
+    type: String,
+    required: true, // tenemos validacion con express validator pero esto es para la DB
+    trim: true
+  },
+  clientName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  }
+})
+
+// aqui definimos el modelo y se registra en la instancia de mongoos
+const Project = mongoose.model<ProjectType>('Project', ProjectSchema)
+export default Project
