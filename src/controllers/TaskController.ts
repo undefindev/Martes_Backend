dc3fbf5b19912c3a608699e6016223519d0670b2
alex bedolla <undefindev@outlook.com>
2024-04-26 19:32:06 -0700

    Promise.allSettled

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -7,8 +7,9 @@ export class TaskController {
       const task = new Task(req.body) // creamos la instancia y despues
       task.project = req.project.id // validamos si el projecto existe y le asignamos la tarea
       req.project.tasks.push(task.id) // con esto le agregamos la tarea al projecto pero nada mas enla memoria
-      await task.save() // aqui guardamos la tarea en la DB
-      await req.project.save() // aqui ya lo estamos almacenando en la DB
+      /* await task.save() */ // aqui guardamos la tarea en la DB
+      /* await req.project.save() */ // aqui ya lo estamos almacenando en la DB
+      await Promise.allSettled([task.save(), req.project.save()]) // aqui estamos optimisando el codigo con 'Promise.allSettled'
       res.send('tarea creada')
     } catch (error) {
       console.log(error)
