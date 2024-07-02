fb62cc74dcbbdb17bf221ac02cb3bbdd0eccddd5
alex bedolla <undefindev@outlook.com>
2024-07-01 19:32:57 -0700

    verificando el jwt en el backend

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/User";
 
 export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
   const bearer = req.headers.authorization
@@ -9,6 +11,19 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
 
   // para traernos el token del bearer
   const [, token] = bearer.split(' ')
+
+  // el decode del jwt
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET) // revisa que el token sea valido.. que no expiro
+
+    if (typeof decoded === 'object' && decoded.id) {
+      const user = await User.findById(decoded.id) // y tambien revisamos en la DB que el usuario existe
+      console.log(user)
+    }
+
+  } catch (error) {
+    res.status(500).json({ error: 'el Maldito Token no es valido perro' })
+  }
 
   next()
 
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -12,7 +12,7 @@ export const generateJWT = (payload: UserPayload) => {
   } */
 
   const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: '6m'
+    expiresIn: '180d'
   });
 
   return token
